714980e5e521a360088e4f7b03d6bfd841148a17
pure-adachi <takuya_adachi@pure-system.com>
2022-01-09 17:23:03 +0900

    Add gorm

--- cmd/main.go
+++ cmd/main.go
@@ -3,41 +3,104 @@ package main
 import (
 	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+  "gorm.io/gorm"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+    AllowOrigins: []string{os.Getenv("CORS_ORIGIN")},
+    AllowMethods: []string{"PUT", "PATCH", "DELETE"},
+  }))
+
 	r.GET("/api/sample", getHelloWorld)
 	r.GET("/api/todos", getTodos)
+	r.POST("/api/todos", addTodo)
+	r.PATCH("/api/todos/:Id", updateTodo)
+	r.DELETE("/api/todos/:Id", deleteTodo)
 
 	r.Run(":" + port)
 }
 
-func getHelloWorld(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", os.Getenv("CORS_ORIGIN"))
+type Todo struct {
+	gorm.Model
+	Id    uint64 `gorm:"primaryKey;autoIncrement`
+	Title string
+}
 
+func gormConnect() *gorm.DB {
+	dsn := os.Getenv("DATABASE_URL")
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Todo{})
+
+	return db
+}
+
+func getHelloWorld(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H { "message": "Hello World!" })
 }
 
 func getTodos(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", os.Getenv("CORS_ORIGIN"))
+	var todos [] Todo
 
-	type Todo struct {
-		Id int
+	db := gormConnect()
+
+	db.Find(&todos)
+
+	c.JSON(http.StatusOK, gin.H { "todos": todos })
+}
+
+func addTodo(c *gin.Context) {
+	type InputTodo struct {
 		Title string
 	}
 
-	todos := [...] Todo {
-		{ Id: 1, Title: "TODO 1" },
-		{ Id: 2, Title: "TODO 2" },
-		{ Id: 3, Title: "TODO 3" },
-		{ Id: 4, Title: "TODO 4" },
-		{ Id: 5, Title: "TODO 5" },
+	var inputTodo InputTodo
+	c.BindJSON(&inputTodo)
+
+	newTodo := Todo { Title: inputTodo.Title }
+
+	db := gormConnect()
+
+	db.Create(&newTodo)
+}
+
+func updateTodo(c *gin.Context) {
+	type InputTodo struct {
+		Title string
 	}
 
-	c.JSON(http.StatusOK, gin.H { "todos": todos })
+	Id := c.Param("Id")
+	id, _ := strconv.Atoi(Id)
+
+	db := gormConnect()
+
+	var todo Todo
+
+	db.First(&todo, id)
+
+	var inputTodo InputTodo
+	c.BindJSON(&inputTodo)
+
+	db.Model(&todo).Update("Title", inputTodo.Title)
+}
+
+func deleteTodo(c *gin.Context) {
+	Id := c.Param("Id")
+	id, _ := strconv.Atoi(Id)
+
+	db := gormConnect()
+
+	var todo Todo
+
+	db.Delete(&todo, id)
 }
